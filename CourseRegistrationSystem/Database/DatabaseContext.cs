276fa7c567a9f9d0bf649bf1d8c20c9abd3d7e23
callistachang <changcallista@gmail.com>
2020-05-01 14:24:07 +0800

    Base EF core implementation

---
+++ CourseRegistrationSystem/Database/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CourseRegistrationSystem.Model;
+
+namespace CourseRegistrationSystem.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Student> Students;
+
+        public DbSet<Account> Accounts;
+
+        public DbSet<IUser> Users;
+
+
+    }
+}
---
+++ CourseRegistrationSystem/Database/DatabaseManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseRegistrationSystem.Database
+{
+    public class DatabaseManager
+    {
+        private DatabaseContext Database;
+
+        public void Initialize()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MySQL"].ConnectionString;
+            var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
+            optionsBuilder.UseMySql(connectionString);
+            optionsBuilder.EnableSensitiveDataLogging(true);
+            Database = new DatabaseContext(optionsBuilder.Options);
+        }
+    }
+}
