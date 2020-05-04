39870d03c9863fc7d0ebee03e57d3282ac705d64
callistachang <changcallista@gmail.com>
2020-05-05 00:03:51 +0800

    Create test suite

---
+++ CourseRegistrationSystem.Tests/TestDatabase.cs
@@ -0,0 +1,21 @@
+﻿using CourseRegistrationSystem.Controller;
+using CourseRegistrationSystem.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Xunit;
+
+namespace CourseRegistrationSystem.Tests
+{
+    public class TestDatabase
+    {
+        private readonly SystemDatabase Database;
+
+        public TestDatabase()
+        {
+            Database = new SystemDatabase();
+            var optionsBuilder = new DbContextOptionsBuilder<SystemContext>();
+            optionsBuilder.UseInMemoryDatabase("TestDatabase");
+            Database.Initialize(optionsBuilder);
+        }
+    }
+}
