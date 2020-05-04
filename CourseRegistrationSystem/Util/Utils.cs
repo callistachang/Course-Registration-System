5aff0f08552dd5441a43b9d00060eb96d511276f
callistachang <changcallista@gmail.com>
2020-05-05 03:09:10 +0800

    Fix utils bug

--- CourseRegistrationSystem/Util/Utils.cs
+++ CourseRegistrationSystem/Util/Utils.cs
@@ -12,82 +12,29 @@ namespace CourseRegistrationSystem
     {
         public static readonly Encoding Encoding = Encoding.UTF8;
         public static readonly TimeSpanToStringConverter TimeSpanConverter = new TimeSpanToStringConverter();
-
-        //public static void PrintEnumAsList<T>()
-        //{
-        //    var enums = Enum.GetValues(typeof(T));
-        //    for (int i = 0; i < enums.Length; i++)
-        //    {
-        //        Console.WriteLine("({0}) {1}", i + 1, enums.GetValue(i));
-        //    }
-        //}
+        public static readonly int SaltSize = 32;
 
         public static string GenerateGuid()
         {
             return Guid.NewGuid().ToString();
         }
 
-        public static bool ReadEnumChoice<T>(out T value)
-        {
-            value = default;
-            var enums = Enum.GetValues(typeof(T));
-            for (int i = 0; i < enums.Length; i++)
-            {
-                Console.WriteLine("({0}) {1}", i + 1, enums.GetValue(i));
-            }
-
-            Console.Write("Choose option: ");
-            int choice;
-            if (int.TryParse(Console.ReadLine(), out choice) && choice <= enums.Length)
-            {
-                value = (T)enums.GetValue(choice - 1);
-            }
-            return value != null;
-        }
-
-        public static string ReadPassword()
-        {
-            string password = "";
-            ConsoleKeyInfo key;
-            do
-            {
-                key = Console.ReadKey(true);
-
-                if (!char.IsControl(key.KeyChar) && password.Length < 20)
-                {
-                    password += key.KeyChar;
-                    Console.Write("*");
-                }
-                else
-                {
-                    if (key.Key == ConsoleKey.Backspace && password.Length > 0)
-                    {
-                        password = password.Remove(password.Length - 1);
-                        Console.Write("\b \b");
-                    }
-                }
-            } while (key.Key != ConsoleKey.Enter);
-
-            Console.Write(Environment.NewLine);
-            return password;
-        }
-
-        public static byte[] GenerateSalt()
+        public static string GenerateSalt()
         {
             using (var rng = new RNGCryptoServiceProvider())
             {
-                byte[] salt = new byte[32];
+                byte[] salt = new byte[SaltSize];
                 rng.GetBytes(salt);
-                return salt;
+                return BitConverter.ToString(salt).ToLower().Replace("-", "").Substring(0, SaltSize);
             }
         }
 
-        public static byte[] HashPasswordWithSalt(byte[] plaintext, byte[] salt)
+        public static string HashPasswordWithSalt(string plaintext, string salt)
         {
             using (var sha256 = SHA256.Create())
             {
-                byte[] hash = Combine(plaintext, salt);
-                return sha256.ComputeHash(hash);
+                byte[] hash = Combine(Encoding.GetBytes(plaintext), Encoding.GetBytes(salt));
+                return BitConverter.ToString(sha256.ComputeHash(hash)).ToLower().Replace("-", "").Substring(0, SaltSize);
             }
         }
 
